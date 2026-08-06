965bec4379e52bdf7df67706eb0d768806f6dcc2
mumbaiops45 <mumbaiops45@gmail.com>
2026-08-06 18:25:05 +0530

    Fix build output and add basePath

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
   output: "export",
   images: {
     unoptimized: true,
   },
+  distDir: 'out',  // Add this line
+  basePath: '/samaratglobal',  // Add this line
+  assetPrefix: '/samaratglobal/',  // Add this line
 };
 
 export default nextConfig;
